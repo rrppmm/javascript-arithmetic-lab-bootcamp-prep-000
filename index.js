d961c3346a1f5e4713bafcfbb482a04798df2ff0
Paul M <paulmcadory@gmail.com>
2019-09-25 01:14:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-function inc(n) {
+function inc(a) {
   return a++;
 }
-function dec(n) {
+function dec(a) {
   return a--;
 }
